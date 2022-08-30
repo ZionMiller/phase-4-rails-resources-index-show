31cc7178c9ef3d5cd481d79eeb8ad04f6e223f9b
Zion Miller <djzionmiller@gmail.com>
2022-08-30 15:09:20 -0700

    commit tree 7d37f299068d6e96e248e124706a12c560ee45df

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+  # resources makes a GET, POST, GET (to :id), PATCH, PUT, DELETE
+
+  resources :birds, only: [:index, :show]
+  # only will limit the actions it looks for just to the specified ones in bracket
+
 end
